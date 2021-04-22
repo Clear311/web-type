41fa2d23a4dcd1238279ce1769fe74ee2f322121
Clear311 <44281330+Clear311@users.noreply.github.com>
2021-04-21 21:18:13 -0400

    update

--- variable_font/expand.js
+++ variable_font/expand.js
@@ -1,8 +1,43 @@
 // let x = 1
 // console.log("x:", x)
-document.getElementById("Stomach").addEventListener("click", functionOne)
+//slidebar//
+$('.axis-range').on('input', function(){
 
-let a = 0
-function myFunction() {
-  math.a++
+  let value = parseInt($(this).val()); //get slider input value
+  // console.log(value);
+
+  const slidertype = $(this).data('type'); //get slider axis
+
+  // console.log("--"+slidertype, value );
+
+  $('#sidebar').css("--"+slidertype, value);
+
+});
+
+//click to feed//
+//declare initial variable as 0 (the recipe!)
+var a = 0;
+
+//each time we click our text, variable 'a' will increase by 1
+document.getElementById("text").onclick = function(){
+  a++;
 }
+//wrapping everything else around this checkValue() function which checks every .05 seconds if anything has changed
+function checkValue() {
+//at initial value, does nothing
+  if (a === 0) {
+//when 'a' changes to 1 after a click, add class '.animate' to our text which changes the font-variation-settings
+  } else if (a === 1) {
+    var classAdd= document.getElementById("text");
+    classAdd.className += " animate";
+//if a is neither 0 or 1, add 'animate2' class (doing the same thing as above but with different animation keyframe values)
+  } else {
+    var classAdd= document.getElementById("text");
+    classAdd.className += " animate2";
+  }
+//checks if the value of variable a changed every 0.05 seconds with .setTimeout.
+  window.setTimeout(checkValue, 50);
+}
+//Finally, we run the function we just declared
+checkValue();
+
